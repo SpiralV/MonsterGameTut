5dc2545f9ccab7f39b5af1ca7ba2dcc3d2e91149
SpiralV <MVelasco90@hotmail.com>
2021-07-26 15:44:30 -0700

    encoded my gitignore wrong now its working wowwy

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MonsterGameTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
